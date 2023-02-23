9ce310d0f4757471521afeddd85018abaf189591
Yuriy <yuriykonyukh@gmail.com>
2023-02-23 18:31:18 +0300

    feat: add solution for guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,17 @@
 class GuessingGame {
     constructor() {}
-
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
-
+    /* Use Binary Search for finding half of range of numbers */
     guess() {
-
+        this.guessTheNumber = Math.ceil( (this.min + this.max) / 2);
+        return this.guessTheNumber;
     }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+    /*If guessing number is lower we move right side of numbers*/
+    lower() { this.max = this.guessTheNumber; }
+    /*If guessing number is greater we move left side of numbers*/
+    greater() { this.min = this.guessTheNumber; }
 }
-
 module.exports = GuessingGame;
